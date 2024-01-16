8b698420fef9859d84662763d5a4eded3b39daba
Lana Kulakova <lanakulakova@gmail.com>
2024-01-16 23:29:26 +0300

    first commit

---
+++ src/MyMealsAndIngredients.js
@@ -0,0 +1,42 @@
+const MyMealsAndIngredients = ({selectedDay, updateDay}) => {
+
+
+    const editMyMeal = (myInput, value) => {
+        updateDay ({
+            ...selectedDay,
+            [myInput]: value
+        })
+    }
+    
+    
+    if (!selectedDay) return <p> With this application you can have your meals plan at hand! </p>
+        return <div className="whole-plan">
+            <input
+                type="text"
+                className="myInput"
+                placeholder="Today is..."
+                id="title"
+                value={selectedDay.title}
+                onChange = { (e) => editMyMeal("title", e.target.value)}
+                />
+    
+                <textarea 
+                placeholder="Write your meal plan here"
+                id= "mealForADay"
+                value={selectedDay.mealForADay}
+                onChange = { (e) => editMyMeal("mealForADay", e.target.value)}
+                />
+    
+                <textarea 
+                placeholder="List of Ingredients"
+                id= "mealForADay"
+                value={selectedDay.ingredients}
+                onChange = { (e) => editMyMeal("ingredients", e.target.value)}
+                />
+            
+            
+        </div>
+    
+    }
+    
+    export default MyMealsAndIngredients;
